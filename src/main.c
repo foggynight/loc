48b592c18d311138167904d156c8eead6cf58c9a
foggynight <foggynight709@gmail.com>
2021-07-26 23:45:26 -0230

    Add option to pass filenames as command line args
    - Added the option pass filenames as command line arguments to loc
      rather than only using stdin to get input
    - Renamed read_file to parse_file

--- src/main.c
+++ src/main.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void read_file(FILE *file,
-			   size_t *loc,
-			   size_t *sloc,
-			   size_t *bytes);
+void parse_file(FILE *file,
+				size_t *loc,
+				size_t *sloc,
+				size_t *bytes);
 
-int main(void)
+int main(int argc, char **argv)
 {
 	size_t loc = 0;             // Lines of code
 	size_t sloc = 0;            // Non-empty lines of code
 	size_t bytes = 0;           // Total size in bytes
 
-	read_file(stdin, &loc, &sloc, &bytes);
+	FILE *file;
+
+	if (argc > 1) {
+		for (int i = 1; i < argc; ++i) {
+			file = fopen(argv[i], "r");
+			if (!file) {
+				fprintf(stderr, "loc: Cannot open file: %s", argv[i]);
+				return 1;
+			}
+			parse_file(file, &loc, &sloc, &bytes);
+		}
+	}
+	else {
+		parse_file(stdin, &loc, &sloc, &bytes);
+	}
 
 	printf("loc:\t%ld\n"
 		   "sloc:\t%ld\n"
@@ -22,10 +36,10 @@ int main(void)
 	return 0;
 }
 
-void read_file(FILE *file,
-               size_t *loc,
-               size_t *sloc,
-               size_t *bytes)
+void parse_file(FILE *file,
+				size_t *loc,
+				size_t *sloc,
+				size_t *bytes)
 {
 	char *line = NULL;
 	size_t line_size = 0;
